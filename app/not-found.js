890a8abbfa0f61b281422dffb24cffbc263c3487
sandrbuz <sandrbuz@mail.ru>
2024-11-24 23:03:05 +0300

    changed the redirect and added a meta tag to not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,23 +1,24 @@
 "use client"
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
+import { redirect } from "next/navigation";
 
 
 export default function NotFound() {
-  const router = useRouter();
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      router.push('/');
+  useEffect(() => {
+    document.title = "404 error";
+    setTimeout(() => {
+      redirect('/')
     }, 3000)
-  },[])
+  }, [])
 
-    return (
-      <div>
-        <h1>404 - Страница не найдена</h1>
-        <p>К сожалению, запрашиваемая страница не существует.</p>
-      </div>
-    );
-  }
+
+
+  return (
+    <div>
+      <h1>404 - Страница не найдена</h1>
+      <p>К сожалению, запрашиваемая страница не существует.</p>
+    </div>
+  );
+}
