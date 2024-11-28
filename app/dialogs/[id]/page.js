912088c85cbe034e96ae081bb11777c9b1786a81
sandrbuz <sandrbuz@mail.ru>
2024-11-28 04:30:55 +0300

    added error handling when routing chats to dialogsPage

--- app/dialogs/[id]/page.js
+++ app/dialogs/[id]/page.js
@@ -1,40 +1,49 @@
-'use client'
+'use client';
 
 import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { addMsg } from "@/store/dialogsSlice";
-import { useParams } from "next/navigation";
-
-
+import { useParams, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 function DialogsPage() {
-
   const dispatch = useDispatch();
+  const router = useRouter();
   const [newMessageText, setNewMessageText] = useState('');
-
   const params = useParams();
 
   const chatNames = useSelector((state) => Object.keys(state.dialogs.messages));
   const chatName = chatNames[params.id - 1]; 
 
-  const messages = useSelector((state) => state.dialogs.messages[chatName] );
+  // Если chatName не существует, перенаправляем на 404
+  useEffect(() => {
+    if (!chatName) {
+      redirect('/404') // Или отобразите кастомную страницу ошибки
+    }
+  }, [chatName, router]);
 
+  const messages = useSelector((state) => state.dialogs.messages[chatName] || []);
 
-  const handeSendMessage = () => {
-      dispatch(addMsg({chatName, message: newMessageText}))
-      setNewMessageText("")
-      console.log()
-  }
+  const handleSendMessage = () => {
+    if (!newMessageText.trim()) return;
+    dispatch(addMsg({ chatName, message: newMessageText }));
+    setNewMessageText('');
+  };
 
-
+  // Если chatName отсутствует, ничего не рендерим
+  if (!chatName) return null;
 
   return (
     <>
       {messages.map((msg) => (<div key={msg.key}>{msg.message}</div>))}
-      <input value={newMessageText} type="text" onChange={(e) => setNewMessageText(e.target.value)} />
-      <button onClick={handeSendMessage}>send</button>
+      <input
+        value={newMessageText}
+        type="text"
+        onChange={(e) => setNewMessageText(e.target.value)}
+      />
+      <button onClick={handleSendMessage}>send</button>
     </>
-  )
+  );
 }
 
 export default DialogsPage;
