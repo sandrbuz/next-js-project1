79860fea099388e47a3c0b92f57ad60288b2d462
sandrbuz <sandrbuz@mail.ru>
2025-04-17 19:01:31 +0300

    установил линтер в проект командой (автоматически создался файл линтера)

--- app/users/UsersPage.js
+++ app/users/UsersPage.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
 
-
-
-const UsersPage = ({users}) => {
-
-    return (
-        <>
-            <h1>Users:</h1>
-            <p>SSR dynamic routing</p>
-            {users.map((user)=>{
-              return <Link href={`/users/${user.id}`} key={user.name}>{user.name}</Link>
-            })}
-        </>
-    )
-}
+const UsersPage = ({ users }) => {
+  return (
+    <>
+      <h1>Users:</h1>
+      <p>SSR dynamic routing</p>
+      {users.map((user) => {
+        return (
+          <Link href={`/users/${user.id}`} key={user.name}>
+            {user.name}
+          </Link>
+        );
+      })}
+    </>
+  );
+};
 
 export default UsersPage;
