23090ef8aba9436176dee6d1df07c437ac8cadbe
sandrbuz <sandrbuz@mail.ru>
2025-04-17 19:21:03 +0300

    removed unnecessary linter rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,22 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
   pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    rules: {
+      "react/prop-types": "off", // ❌ отключаем требование PropTypes
+      "react/react-in-jsx-scope": "off", // ✅ отключили требование React в JSX
+    },
+  },
 ]);
