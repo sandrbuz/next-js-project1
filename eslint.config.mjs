6b452a730c810e03c8b81a3b9e2025f8804040fb
sandrbuz <sandrbuz@mail.ru>
2025-04-21 03:31:57 +0300

    Добавил коммент для eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,11 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  //устанавливал линтер командой npx eslint --init (как сказал гпт, не совсем как из статьи), раньше я читал в статье при утановке предлагалось
+  //выбрать готовый набор правил например из airbnb, но теперь не предлагает airbnb, а делает свои стандартные правила.
+  //airbnb не поддерживает эту новую конфигурацию (только старый .eslintrc.json ), но при желании можно скопировать правила airbnb
+  //и интегрировать сюда(в теории) в next-js-project3 установил eslint командой из офиц доки eslint. Потом с курсором интегрировал туда airbnb
+
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
@@ -13,7 +18,7 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: { globals: globals.browser },
   },
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat.recommended, //все что ниже, давил я сам.
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     rules: {
